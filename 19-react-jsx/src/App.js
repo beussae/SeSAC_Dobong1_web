75c01a34560052dfc68b283f92859340b2f47534
beussae <dbseowjd123@naver.com>
2024-04-07 21:07:21 +0900

    20240315react-jsx

---
+++ 19-react-jsx/src/App.js
@@ -0,0 +1,16 @@
+// import logo from './logo.svg';
+import "./App.css";
+import UseJSX from "./UseJSX";
+import PracticeJSX from "./PracticeJSX";
+
+function App() {
+  return (
+    <div>
+      <h1>JSX 문법을 배워봐요!</h1>
+      <UseJSX></UseJSX>
+      <PracticeJSX></PracticeJSX>
+    </div>
+  );
+}
+
+export default App;
