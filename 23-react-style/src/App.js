dd5211f0db245dccf8fd7f844bc9b9e374b9ffb9
beussae <dbseowjd123@naver.com>
2024-04-07 21:38:11 +0900

    20240323-react-style

---
+++ 23-react-style/src/App.js
@@ -0,0 +1,19 @@
+import AppCopy from "./AppCopy";
+import BasicCSS from "./components/BasicCss";
+import ModuleCss from "./components/ModuleCss";
+import Sass from "./components/Sass";
+import StyledComp from "./components/styledComp";
+
+function App() {
+  return (
+    <>
+      {/* <BasicCSS /> */}
+      {/* <ModuleCss /> */}
+      {/* <StyledComp /> */}
+      {/* <AppCopy /> */}
+      <Sass />
+    </>
+  );
+}
+
+export default App;
